30c85a73aabc7d4da76c30600f4f2bc4a2886b5b
Sharon CR <schullunquia@unsa.edu.pe>
2022-02-02 23:15:48 -0500

    Add hellow world and first exercise

---
+++ Leccion-01/01-HolaMundo.js
@@ -0,0 +1,3 @@
+var mensaje = "Hola Mundo desde JavaScript";
+
+console.log(mensaje);
---
+++ Leccion-01/02-tipos-de-datos.js
@@ -0,0 +1,23 @@
+/* 
+  TIPOS DE DATOS
+*/
+
+// Tipo de dato string
+var nombre = "Sharon";
+console.log(nombre);
+
+// Tipo de dato numerico
+var numero = 1000;
+console.log(numero);
+
+// Tipo de dato objeto
+var objeto = {
+  nombre: "Juan",
+  apellido: "Perez",
+  telefono: "555165",
+};
+console.log(objeto);
+
+nombre = 10;
+console.log(nombre);
+console.log(typeof nombre);
