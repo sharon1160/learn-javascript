b57bb5b4375e67d5a7d16c3836ccdaa3288e4ad9
Sharon CR <schullunquia@unsa.edu.pe>
2022-02-05 16:58:34 -0500

    Add data type boolean, function, symbol, class, undefined and null

--- Leccion-01/02-tipos-de-datos.js
+++ Leccion-01/02-tipos-de-datos.js
@@ -18,6 +18,39 @@ var objeto = {
 };
 console.log(objeto);
 
+// Asignando otro valor a la variable nombre
 nombre = 10;
 console.log(nombre);
 console.log(typeof nombre);
+
+// Tipo de dato boolean (true, false)
+var bandera = false;
+console.log(typeof bandera); // false
+
+// Tipo de dato function
+function miFuncion() {}
+console.log(typeof miFuncion); // function
+
+// Tipo de dato Symbol
+var simbolo = Symbol("mi simbolo");
+console.log(typeof simbolo); // Symbol
+
+// Tipo clase es una function
+class Persona {
+  constructor(nombre, apellido) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+  }
+}
+console.log(typeof Persona); // function
+
+// Tipo undefined
+var x;
+console.log(typeof x); // undefined
+
+x = undefined; // se puede asignar undefined
+console.log(typeof x); // undefined
+
+// Tipo null = ausencia de valor
+var y = null;
+console.log(typeof y); // object
